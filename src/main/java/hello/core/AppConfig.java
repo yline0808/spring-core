ae1f5c36606feb873feeb4029911de27db5ab6c6
yline <ryuyoseon@rysMAC.local>
2021-10-17 15:00:34 +0900

    SRP, DIP, OCP적용
    - Spring 컨테이너를 통한 Bean관리
    - MemberService, OrderService 원칙에 맞게 수정

---
+++ src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,43 @@
+package hello.core;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/*
+역할을 세우고 그 안에 구현이 들어가도록 구성
+이렇게 구현하면 애플리케이션 전체 구성이 어떻게 되어있는지 한눈에 들어온다.
+ */
+
+@Configuration
+public class AppConfig {
+
+    // 생성자 주입
+    @Bean
+    public MemberService memberService(){
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    @Bean
+    public MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
+    public OrderService orderService(){
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy(){
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+    }
+}
--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,7 +3,11 @@ package hello.core.member;
 // 인터페이스가 하나만 있을 경우 관례상 구현체 뒤에 Impl 이라고 많이 씀
 public class MemberServiceImpl implements MemberService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {

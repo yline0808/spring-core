2d374df1ce53d21e38f11201fa0a537d9691d332
yline <ryuyoseon@rysMAC.local>
2021-10-18 15:33:18 +0900

    test component scan
    - 컴포넌트 스캔, 의존관계 자동 주입 테스트
    - 스캔대상, 스캔위치 지정 테스트
    - 필터 테스트
    - 중복&충돌 테스트

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,10 +1,15 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 // 인터페이스가 하나만 있을 경우 관례상 구현체 뒤에 Impl 이라고 많이 씀
+@Component
 public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
